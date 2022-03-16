dd9f97ed34a27a1649cfa28d521ef2d591826dbc
Trutii <Truti-matruti@mail.ru>
2022-03-16 14:58:55 +0700

    Fix

--- src/main/main.c
+++ src/main/main.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
+#include "libmake/circle.h"
+#include "libmake/triangle.h"
 #include <math.h>
-#include <circle.h>
-#include <htriangle.h>
-int main() 
+#include <stdio.h>
+
+int main()
 {
-	int a;
-	while(1)
-	{
-		printf("1. Cirle\n2. Triangle\nChoose your figure: ");
-		scanf(" %d", &a);
-		if (a == 1)
-		{
-			circle();
-			break;
-		}
-		if (a == 2)
-		{
-			triangle();
-			break;
-		}
-		printf("Error\n");
-		return 0;
-		}
-	}
-	return 0;
+    int a;
+    while (1) {
+        printf("1. Cirle\n2. Triangle\nChoose your figure: ");
+        scanf(" %d", &a);
+        if (a == 1) {
+            circle();
+            break;
+        }
+        if (a == 2) {
+            triangle();
+            break;
+        }
+        else {
+            printf("Error\n");
+            return 0;
+        }
+    }
+    return 0;
 }
