c727e8e896e0a5443ca55e0181222ecc07a2ec1f
Trutii <Truti-matruti@mail.ru>
2022-04-04 12:25:07 +0700

    Clean trash files

--- src/main/main.c
+++ src/main/main.c
@@ -1,25 +1,9 @@
-#include "libmake/circle.h"
-#include "libmake/triangle.h"
+#include "libmake/Read.h"
 #include <math.h>
 #include <stdio.h>
 
 int main()
 {
-    int a;
-    while (1) {
-        printf("1. Cirle\n2. Triangle\nChoose your figure: ");
-        scanf(" %d", &a);
-        if (a == 1) {
-            circle();
-            break;
-        }
-        if (a == 2) {
-            triangle();
-            break;
-        } else {
-            printf("Error\n");
-            return 0;
-        }
-    }
+    Read();
     return 0;
 }
